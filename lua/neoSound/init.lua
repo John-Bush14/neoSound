495a113e369b9ed384e520d4a3758f215a33cd6e
John-Bush14 <joppe@stuyck.be>
2024-04-04 18:14:36 +0200

    Revert "feat: local file importion"
    This reverts commit 86b438b670262be28ca8be690841823a4c9c52ac.

--- lua/neoSound/init.lua
+++ lua/neoSound/init.lua
@@ -1,53 +1,25 @@
 local M = {}
 local default_provider = "local"
-local uv = vim.uv
-local pack_folder = "soundpacks/"
-vim.cmd("set shellquote=\"")
 
 local import_sound = {
-   ["local"] = function(sound, destination) -- local file
-      function import_source(source, destination) 
-	 if string.match(source, "#") then return end
-         local command = string.format("!cp %s ~/%s", vim.fn.shellescape(source), destination)
-         vim.cmd(command) 
-      end
-
-      if not (string.sub(sound, -1) == "/") then 
-         import_source(sound, destination)
-         return
-      end
-
-      local dir = uv.fs_scandir(string.sub(sound, #sound-1))
-
-      if dir then 
-         repeat 
-            local file_name, type = uv.fs_scandir_next(dir)
-            if file_name and type == "file" then
-               import_source(sound .. file_name, destination)
-            end
-         until not file_name
-      end
+   ["local"] = function(sound) -- local file
    end,
-
-   ["youtube"] = function(sound, destination) end,
+   ["youtube"] = function(sound) end,
+   ["spotify"] = function(sound) end
 }
 
 function M.setup(soundpacks)
    if soundpacks == nill then return end
-   if vim.fn.isdirectory(pack_folder) == 0 then
-      vim.fn.mkdir(pack_folder, 'p')
-   else 
-      vim.fn.system("rm -rf " .. pack_folder)
-      vim.fn.mkdir(pack_folder) 
-   end
-
-   for _k, soundpack in pairs(soundpacks) do
+   for soundpack_index = 1, #soundpacks do
+      local soundpack = soundpacks[soundpack_index]
       local sounds = soundpack["sounds"]
-      vim.fn.mkdir(pack_folder .. soundpack["name"])
-      for k, sound in pairs(sounds) do
-         local provider = "local"
-         if string.match(sound, "youtube.com") then provider = "youtube" end
-         import_sound[provider](sound, pack_folder .. soundpack["name"])
+
+      for sound_index = 1, #sounds do
+         local sound = sounds[sound_index]
+	      if soundpack["provider"] == nil then
+	         soundpack["provider"] = default_provider
+         end
+         import_sound[soundpack["provider"]](sound)
       end
    end
 end
